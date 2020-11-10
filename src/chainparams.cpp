96b2f585fcaec778745f9cf65aff493a80c38bae
Your Name <you@example.com>
2020-11-10 17:25:23 +0200

    ccdd

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -5,7 +5,7 @@
 //Copyright (c) 2020 The Poriun Coin developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
+// ---
 #include "chainparams.h"
 
 #include "chainparamsseeds.h"
