7ae932f87ef2255a04900485d6fa71b398dbfec6
Your Name <you@example.com>
2020-11-11 19:26:47 +0200

    cc

--- src/pow.cpp
+++ src/pow.cpp
@@ -135,10 +135,10 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits)
 
     // Check proof of work matches claimed amount
    
-   
-   if (hash > bnTarget)
+   /*
+     if (hash > bnTarget)
         return error("CheckProofOfWork() : hash doesn't match nBits"); 
-
+        */
 
 
     return true;
